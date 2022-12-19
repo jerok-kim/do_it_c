dd28122524225513cf7c71a6fb25c512c67b50a0
jerok <jerok.kim@gmail.com>
2022-12-19 23:43:06 +0900

    Update chapter_11.md

---
+++ code/ch11/11_01_01/main.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int Sum(int data1, int data2) {  // 매개변수 data1, data2는 Sum 함수의 지역 변수
+    // Sum 함수의 지역 변수 result를 선언한 후 data + data2 값으로 초기화함
+    int result = data1 + data2;
+    return result;
+}
+
+void main() {
+    int result;  // main 함수의 지역 변수 result. 초기화되지 않아 어떤 값이 들어 있는지 모름
+    result = Sum(5, 3);
+    printf("5 + 3 = %d\n", result);
+}
---
+++ code/ch11/11_02_02/main.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int result;  // 전역 변수로 특별한 초기화 값이 없으면 0으로 초기화된다
+
+void Sum(int data1, int data2) {
+    int result;  // 지역 변수로 초기화되어 있지 않다
+    // 지역 변수의 우선순위가 높기 때문에 연산 결과 값은 전역 변수 result가 아닌 지역 변수 result에 저장된다
+    result = data1 + data2;
+}
+
+void main() {
+    Sum(5, 3);
+    printf("5 + 3 = %d\n", result);
+}
