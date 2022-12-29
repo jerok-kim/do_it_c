055a13e2b5ae4ccf7664d2b09b64b8dd3a6c63bc
jerok <jerok.kim@gmail.com>
2022-12-29 12:20:21 +0900

    Update: chapter_17.md

---
+++ code/ch17/17_02_02/main.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <malloc.h>
+
+void main() {
+    short **pp;
+    pp = (short **) malloc(sizeof(short *));  // 4바이트를 할당함. 두 번째 상자 만듦
+    *pp = (short *) malloc(sizeof(short));  // 2바이트를 할당함. 세 번째 상자 만듦
+
+    **pp = 10;  // 2차원 포인터를 사용하여 세 번째 상자에 10을 대입함
+    printf("**pp : %d\n", **pp);  // 10을 출력함
+    free(*pp);  // 세 번째 상자에 해당하는 메모리 해제함
+    free(pp);  // 두 번째 상자에 해당하는 메모리 해제함
+}
