f3b484019edc7cc58484ea8050ca52574369d508
jerok <jerok.kim@gmail.com>
2023-01-01 13:57:53 +0900

    Update: chapter_19.md

---
+++ code/ch19/19_04_05/main.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+void main() {
+    char temp[64];  // 파일에서 문자열을 입력 받을 배열 변수
+
+    FILE *p_file = fopen("jerok.txt", "rt");  // 읽기 모드로 텍스트 파일을 오픈함
+
+    if (NULL != p_file) {  // 파일 열기에 성공한 경우
+
+        /// fgets 함수의 두 번째 매개변수는 첫 번째 매개변수에 사용되는 메모리의 크기를 표기한다
+        while (NULL != fgets(temp, sizeof(temp), p_file)) {
+
+            /// 파일에서 읽은 문자열을 화면에 출력한다.
+            /// fgets 함수는 fscanf 함수와 달리 줄의 끝에 있는 \n도 읽어서 temp 배열 마지막에 넣어 주기 때문에
+            /// printf 함수를 사용할 때 \n을 적지 않는다.
+            printf("%s", temp);
+        }
+        
+        fclose(p_file);
+    }
+}
---
+++ code/ch19/19_05_04/main.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+void main() {
+    int file_size = 0;
+
+    FILE *p_file = fopen("jerok.dat", "rb");  // 읽기 모드로 바이너리 파일을 오픈함
+
+    if (NULL != p_file) {  // 파일 열기에 성공한 경우 
+        fseek(p_file, 0, SEEK_END);  // 파일의 끝을 이동함
+        file_size = ftell(p_file);  // 현재 파일 위치로 파일의 크기를 구함
+        printf("파일 크기: %d\n", file_size);
+        fclose(p_file);  // 파일을 닫음
+    }
+}
